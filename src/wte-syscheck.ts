a52722efda9fd192ab72b6ad48ab07862f2f2c16
Matthew Evans <mevans+git@wtfsystems.net>
2024-06-04 09:14:47 -0400

    sync

--- src/wte-syscheck.ts
+++ src/wte-syscheck.ts
@@ -6,6 +6,21 @@
  * @copyright MIT see LICENSE.md
  */
 
+import { lookpathSync } from 'find-bin'
+import { red, green } from 'kolorist'
 import * as wtf from './_common.js'
 
 wtf.scriptTitle(`WTEngine System Check Utility`)
+
+let res = 0
+
+wtf.config.checkApps.forEach((app:string) => {
+  if(lookpathSync(app)) {
+    console.log(green(`Command '${app}' found`))
+  } else {
+    console.log(red(`Command '${app}' not found!`))
+    res = 1
+  }
+})
+
+process.exit(1)
